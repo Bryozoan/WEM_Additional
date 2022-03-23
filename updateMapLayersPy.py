520dde0bc6b6663358973a05c704daf2fb354b13
JonathonCV <81706070+Bryozoan@users.noreply.github.com>
2022-03-23 16:32:48 -0600

    Added WEM 2&3

--- updateMapLayersPy.py
+++ updateMapLayersPy.py
@@ -59,5 +59,53 @@ def labels(ILMfile):
                                         
     return(tractOwn, QData, dSize, WEMOwn, WEMIOwn, WEMIIOwn, WEMIIIOwn)
     
+
+#Leasholds
+# this will take the Working interest file from ILM and convert it into a CSV 
+# for QGIS.
+def leaseLabel(ILMfile):
+    #Import the data
+    tractLea = pd.read_excel(ILMfile, sheet_name="Well-Bore Only",
+                             header=5)
+    #Remove the tract number so that that they can be summed by section
+    tractLea['Section'] = tractLea['Tract Number'].map(lambda x: str(x)[2:-4])
+    tractLea = tractLea.dropna(axis=0, subset=['Tract Number'])
     
-tractOwn, QData, dSize, WEMOwn, WEMIOwn, WEMIIOwn, WEMIIIOwn = labels(CSVfile+r'\Tract Ownership List.xlsx')
+    
+    WEMI = pd.DataFrame()    
+    WEMI['Section'] = tractLea.Section
+    WEMI['Min. Net Acres'] = tractLea['TRACT Min. Net Acres']
+    WEMI['WEM Uintah, LLC Ownership'] = tractLea['WEM Uintah, LLC Ownership']
+    WEMI = WEMI.dropna(axis=0, subset=['WEM Uintah, LLC Ownership'])
+    WEMI['WEM1 Leasehold'] = WEMI['WEM Uintah, LLC Ownership']*WEMI['Min. Net Acres']
+    WEMI = WEMI[WEMI != 0].dropna(axis=0)
+    WEMI = WEMI.groupby(["Section"])['WEM1 Leasehold'].sum().reset_index()
+    
+    WEM2 = pd.DataFrame()    
+    WEM2['Section'] = tractLea.Section
+    WEM2['Min. Net Acres'] = tractLea['TRACT Min. Net Acres']
+    WEM2['WEM Uintah II, LLC Ownership'] = tractLea['WEM Uintah II, LLC Ownership']
+    WEM2 = WEM2.dropna(axis=0, subset=['WEM Uintah II, LLC Ownership'])
+    WEM2['WEM2 Leasehold'] = WEM2['WEM Uintah II, LLC Ownership']*WEM2['Min. Net Acres']
+    WEM2 = WEM2[WEM2 != 0].dropna(axis=0)
+    WEM2 = WEM2.groupby(["Section"])['WEM2 Leasehold'].sum().reset_index()
+    
+    WEM3 = pd.DataFrame()    
+    WEM3['Section'] = tractLea.Section
+    WEM3['Min. Net Acres'] = tractLea['TRACT Min. Net Acres']
+    WEM3['WEM Uintah III, LLC Ownership'] = tractLea['WEM Uintah III, LLC Ownership']
+    WEM3 = WEM3.dropna(axis=0, subset=['WEM Uintah III, LLC Ownership'])
+    WEM3['WEM3 Leasehold'] = WEM3['WEM Uintah III, LLC Ownership']*WEM3['Min. Net Acres']
+    WEM3 = WEM3[WEM3 != 0].dropna(axis=0)
+    WEM3 = WEM3.groupby(["Section"])['WEM3 Leasehold'].sum().reset_index()
+    
+    WEMI.to_csv(CSVfile+r'\WEM1Leasehold.csv')
+    WEM2.to_csv(CSVfile+r'\WEM2Leasehold.csv')
+    WEM3.to_csv(CSVfile+r'\WEM3Leasehold.csv')
+    
+    return(tractLea, WEMI, WEM2, WEM3)
+
+
+TractLea, WEM1, WEM2, WEM3 = leaseLabel(CSVfile+r'\Working Interest Detail.xlsx')
+
+#tractOwn, QData, dSize, WEMOwn, WEMIOwn, WEMIIOwn, WEMIIIOwn = labels(CSVfile+r'\Tract Ownership List.xlsx')
